90f61e238f8fe1b4aa1e4401ba834ae712d87f0e
leavesC <1990724437@qq.com>
2021-10-23 19:57:12 +0800

    更新依赖库

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # compose_tetris
 
-用 Jetpack Compose 实现一个**俄罗斯方块**
+用 Jetpack Compose 实现一个俄罗斯方块
+
+更多介绍请看这里：[学不动也要学，Jetpack Compose 玩一把俄罗斯方块](https://juejin.cn/post/6974585048762679310)
 
 ![](https://z3.ax1x.com/2021/06/12/25y5sx.gif)
--- app/src/main/java/github/leavesc/compose_tetris/MainActivity.kt
+++ app/src/main/java/github/leavesc/compose_tetris/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import github.leavesc.compose_tetris.logic.Action
 import github.leavesc.compose_tetris.logic.TetrisViewModel
 import github.leavesc.compose_tetris.logic.combinedPlayListener
@@ -21,7 +21,6 @@ import github.leavesc.compose_tetris.ui.TetrisBody
 import github.leavesc.compose_tetris.ui.TetrisButton
 import github.leavesc.compose_tetris.ui.TetrisScreen
 import github.leavesc.compose_tetris.ui.theme.ComposeTetrisTheme
-import github.leavesc.compose_tetris.utils.StatusBarUtil
 
 /**
  * @Author: leavesC
@@ -33,56 +32,74 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        StatusBarUtil.transparentStatusBar(this)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            val tetrisViewModel = viewModel<TetrisViewModel>()
-            val lifecycle = LocalLifecycleOwner.current.lifecycle
-            DisposableEffect(key1 = Unit) {
-                val observer = object : DefaultLifecycleObserver {
-                    override fun onResume(owner: LifecycleOwner) {
-                        tetrisViewModel.dispatch(Action.Resume)
-                    }
-
-                    override fun onPause(owner: LifecycleOwner) {
-                        tetrisViewModel.dispatch(Action.Background)
-                    }
-                }
-                lifecycle.addObserver(observer)
-                onDispose {
-                    lifecycle.removeObserver(observer)
-                }
-            }
-            ComposeTetrisTheme {
-                Surface {
-                    val tetrisState by tetrisViewModel.tetrisStateLD.collectAsState()
-                    TetrisBody(tetrisScreen = {
-                        TetrisScreen(tetrisState = tetrisState)
-                    }, tetrisButton = {
-                        TetrisButton(
-                            playListener = combinedPlayListener(
-                                onStart = {
-                                    tetrisViewModel.dispatch(Action.Start)
-                                },
-                                onPause = {
-                                    tetrisViewModel.dispatch(Action.Pause)
-                                },
-                                onReset = {
-                                    tetrisViewModel.dispatch(Action.Reset)
-                                },
-                                onTransformation = {
-                                    tetrisViewModel.dispatch(Action.Transformation(it))
-                                },
-                                onSound = {
-                                    tetrisViewModel.dispatch(Action.Sound)
-                                },
-                            )
-                        )
-                    })
-                }
-            }
-            tetrisViewModel.dispatch(action = Action.Welcome)
+            MainScreen()
         }
     }
+
+    @Composable
+    private fun MainScreen() {
+        val tetrisViewModel = viewModel<TetrisViewModel>()
+        val lifecycle = LocalLifecycleOwner.current.lifecycle
+        DisposableEffect(key1 = Unit) {
+            val observer = object : DefaultLifecycleObserver {
+                override fun onResume(owner: LifecycleOwner) {
+                    tetrisViewModel.dispatch(Action.Resume)
+                }
+
+                override fun onPause(owner: LifecycleOwner) {
+                    tetrisViewModel.dispatch(Action.Background)
+                }
+            }
+            lifecycle.addObserver(observer)
+            onDispose {
+                lifecycle.removeObserver(observer)
+            }
+        }
+        ComposeTetrisTheme {
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setStatusBarColor(
+                    color = Color.Transparent,
+                    darkIcons = false
+                )
+                systemUiController.setNavigationBarColor(
+                    color = Color.Transparent,
+                    darkIcons = false
+                )
+                systemUiController.systemBarsDarkContentEnabled = false
+            }
+            Surface {
+                val tetrisState by tetrisViewModel.tetrisStateLD.collectAsState()
+                TetrisBody(tetrisScreen = {
+                    TetrisScreen(tetrisState = tetrisState)
+                }, tetrisButton = {
+                    TetrisButton(
+                        playListener = combinedPlayListener(
+                            onStart = {
+                                tetrisViewModel.dispatch(Action.Start)
+                            },
+                            onPause = {
+                                tetrisViewModel.dispatch(Action.Pause)
+                            },
+                            onReset = {
+                                tetrisViewModel.dispatch(Action.Reset)
+                            },
+                            onTransformation = {
+                                tetrisViewModel.dispatch(Action.Transformation(it))
+                            },
+                            onSound = {
+                                tetrisViewModel.dispatch(Action.Sound)
+                            },
+                        )
+                    )
+                })
+            }
+        }
+        tetrisViewModel.dispatch(action = Action.Welcome)
+    }
+
 }
 
 @Composable
